9a767b531a6eabd039d2ab3d675d2112c89e9089
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-13 07:47:38 +0900

    list 2-6 p.59

--- hello.php
+++ hello.php
@@ -3,12 +3,11 @@
 echo "整数を入力：";
 $a = trim(fgets(STDIN));
 $result = false;
-$n = 2;
-while ($n <= $a / 2) {
-	if ($a % $n == 0) {
+$n = (int)($a / 2);
+for ($i = 2; $i <= $n; $i++) {
+	if ($a % $i == 0) {
 		$result = true;
 	}
-	$n++;
 }
 if ($result) {
 	echo $a . "は、素数じゃないです。";
