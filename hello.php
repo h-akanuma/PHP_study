e49078e3e7f53f956283779ec84fe5c5bcfc7d4c
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-13 07:36:58 +0900

    list 2-4 p.53

--- hello.php
+++ hello.php
@@ -1,13 +1,22 @@
 <?php
 
-echo "整数を入力：";
+echo "血液型を大文字で入力：";
 $a = trim(fgets(STDIN));
-$b = $a % 2;
-
-if ($b == 0) {
-	echo $a . "は、偶数です。";
-} else {
-	echo $a . "は、奇数です。";
+switch($a) {
+	case "A":
+		echo "あなたは、几帳面なしっかり者でしょう。";
+		break;
+	case "B":
+		echo "あなたは、常識に縛られない自由人でしょう。";
+		break;
+	case "AB":
+		echo "あなたは1つに割り切れない複雑な性格のようです。";
+		break;
+	case "O":
+		echo "あなたは、おおらかで大人物の風格があります。";
+		break;
+	default:
+		echo "あなたは、たぶん人間じゃないです。";
 }
 
 ?>
