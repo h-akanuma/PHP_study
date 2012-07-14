fd78b094eea4eb7521e33f9da3b32b91b2fbbcb6
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 20:57:54 +0900

    list 4-4 p.131

--- hello.php
+++ hello.php
@@ -1,8 +1,25 @@
 <?php
 
+function replace ($str, $f, $r) {
+	$n = mb_strpos($str, $f, 0, 'utf-8');
+	$s = mb_substr($str, 0, $n, 'utf-8') 
+		 . $r 
+		 . mb_substr(
+				$str, 
+				$n + mb_strlen($f, 'utf-8'), 
+				mb_strlen($str, 'utf-8'), 
+				'utf-8'
+		   );
+	return $s;
+}
+
 echo "テキストを入力：";
 $a = trim(fgets(STDIN));
-echo mb_strpos($a, 'PHP', 0, 'utf-8') . "文字目にPHPがあります。";
+echo "検索テキストを入力：";
+$b = trim(fgets(STDIN));
+echo "置換テキストを入力：";
+$c = trim(fgets(STDIN));
+echo "[置換済みテキスト]\n" . replace($a, $b, $c);
 
 ?>
 
