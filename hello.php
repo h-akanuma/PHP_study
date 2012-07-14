f7a05646c2722160db9d8bb8289a9371ae377173
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 22:39:17 +0900

    list 4-42 p.176

--- hello.php
+++ hello.php
@@ -1,13 +1,7 @@
 <?php
 
-function showInfo($path) {
-	echo "Path: " . realpath($path) . "\n";
-	echo "Type: " . filetype($path) . "\nSize: " . filesize($path) . "\n";
-	echo "Modified: " . date('Y-m-d H:i:s', filemtime($path)) . "\nAccessed: " . date('Y-m-d H:i:s', fileatime($path));
-}
-
-echo "ファイル名を入力：";
+echo "フォルダ名を入力：";
 $a = trim(fgets(STDIN));
-showInfo($a);
+mkdir(getcwd() . "/" . $a);
 
 ?>
