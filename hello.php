81aa64eb47cef9faec25f35de759b03baabd48ff
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 22:03:09 +0900

    list 4-29 p.159

--- hello.php
+++ hello.php
@@ -1,14 +1,7 @@
 <?php
 
-function getDiffDate($str) {
-	$d1 = new DateTime("now");
-	$d2 = new DateTime($str);
-	return $d1->diff($d2);
-}
-
-echo "日付を入力：";
+echo "ファイル名を入力：";
 $a = trim(fgets(STDIN));
-$di = getDiffDate($a);
-echo $di->format("%D day %H hours %I minutes.");
+readfile($a);
 
 ?>
