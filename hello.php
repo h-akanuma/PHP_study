79dff4b56d7ea32079e4b6f18fc07519de73b718
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 22:35:33 +0900

    list 4-40 p.175

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 function showInfo($path) {
 	echo "Path: " . realpath($path) . "\n";
 	echo "Type: " . filetype($path) . "\nSize: " . filesize($path) . "\n";
-	echo "Modified: " . filemtime($path) . "\nAccessed: " . fileatime($path);
+	echo "Modified: " . date('Y-m-d H:i:s', filemtime($path)) . "\nAccessed: " . date('Y-m-d H:i:s', fileatime($path));
 }
 
 echo "ファイル名を入力：";
