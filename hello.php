c95467ee790d55cf1184065f3e74deac7d3160ef
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 18:10:57 +0900

    list 3-10 p.120

--- hello.php
+++ hello.php
@@ -1,45 +1,112 @@
 <?php
 
-new MyDataContainer();
-
 abstract class MyData {
 	public $name;
-	abstract function writeData();
+	abstract static public function createData();
 }
 
-class PersonalData extends MyData {
-	public $age = 0;
-	public $address = "";
+interface MyDataOutputable {
+	public function writeData();
+	public function getLabel();
+}
 
-	function __construct($name, $age, $address) {
+class FamilyData extends MyData implements MyDataOutputable {
+	public $age = 0;
+	public $birth = "";
+
+	function __construct($name, $age, $birth) {
 		$this->name = $name;
 		$this->age = $age;
-		$this->address = $address;
+		$this->birth = $birth;
 	}
 
 	function writeData() {
 		echo "[" . $this->name . "]\n";
-		echo "年齢: " . $this->age . "\n";
-		echo "住所:  " . $this->address . "\n\n";
+		echo "年齢：" . $this->age . "\n";
+		echo "誕生日：" . $this->birth . "\n\n";
+	}
+
+	function getLabel() {
+		return $this->name . " [家族]";
+	}
+
+	static public function createData() {
+		echo "名前を入力：";
+		$a = trim(fgets(STDIN));
+		echo "年齢を入力：";
+		$b = trim(fgets(STDIN));
+		echo "誕生日を入力：";
+		$c = trim(fgets(STDIN));
+		$obj = new FamilyData($a, $b, $c);
+		return $obj;
 	}
 }
 
-class BusinessData extends MyData {
-	public $corp = "";
+class PersonalData extends MyData implements MyDataOutputable {
 	public $mail = "";
 	public $tel = "";
 
-	function __construct($name, $corp, $mail, $tel) {
+	function __construct($name, $mail, $tel) {
 		$this->name = $name;
-		$this->corp = $corp;
 		$this->mail = $mail;
 		$this->tel = $tel;
 	}
 
 	function writeData() {
-		echo "[" . $this->name . "@" . $this->corp. "]\n";
-		echo "MAIL: " . $this->mail . "\n";
-		echo "TEL:  " . $this->tel . "\n\n";
+		echo "[" . $this->name . "]\n";
+		echo "メアド：" . $this->mail . "\n";
+		echo "電話 ：" . $this->tel . "\n\n";
+	}
+
+	function getLabel() {
+		return $this->name . " [個人]";
+	}
+
+	static public function createData() {
+		echo "名前を入力：";
+		$a = trim(fgets(STDIN));
+		echo "メールアドレスを入力：";
+		$b = trim(fgets(STDIN));
+		echo "電話番号を入力：";
+		$c = trim(fgets(STDIN));
+		$obj = new PersonalData($a, $b, $c);
+		return $obj;
+	}
+}
+
+class BusinessData extends MyData implements MyDataOutputable {
+	public $corp = "";
+	public $post = "";
+	public $tel = "";
+
+	function __construct ($name, $corp, $post, $tel) {
+		$this->name = $name;
+		$this->corp = $corp;
+		$this->post = $post;
+		$this->tel = $tel;
+	}
+
+	function writeData() {
+		echo "[" . $this->name . "@" . $this->corp . "]\n";
+		echo "POST: " . $this->post . "\n";
+		echo "TEL : " . $this->tel . "\n\n";
+	}
+
+	function getLabel() {
+		return $this->name . " [仕事]";
+	}
+
+	static public function createData() {
+		echo "名前を入力：";
+		$a = trim(fgets(STDIN));
+		echo "会社名を入力：";
+		$b = trim(fgets(STDIN));
+		echo "所属部署を入力：";
+		$c = trim(fgets(STDIN));
+		echo "電話番号を入力：";
+		$d = trim(fgets(STDIN));
+		$obj = new BusinessData($a, $b, $c, $d);
+		return $obj;
 	}
 }
 
@@ -53,57 +120,58 @@ class MyDataContainer {
 
 	function start() {
 		while(true) {
-			echo "検索=1, 入力(p)=2, 入力(b)=3 : ";
+			echo "(f)ind, (i)nput, (l)ist, (d)elete, (q)uit : ";
 			$a = trim(fgets(STDIN));
-			switch($a) {
-				case 1:
+			switch($a){
+				case 'f':
 					echo "検索テキストを入力：";
 					$a = trim(fgets(STDIN));
-					MyDataContainer::findIt($a);
+					$this->findIt($a);
 					break;
-				case 2:
-					$this->addPData();
+				case 'i':
+					echo "タイプを入力[(f)amily, (p)ersonal, (b)usiness]: ";
+					$a = trim(fgets(STDIN));
+					$this->inputData($a);
 					break;
-				case 3:
-					$this->addBData();
+				case 'l':
+					$this->writeList();
 					break;
-				default:
-					exit(0);
+				case 'd':
+					echo "削除する番号を入力j：";
+					$a = trim(fgets(STDIN));
+					$this->deleteIt($a);
+					break;
+				case 'q':
+					echo "終了します。";
+					return;
+					break;
 			}
 			echo "\n";
 		}
 	}
 
-	function addPData() {
-		echo "名前を入力：";
-		$a = trim(fgets(STDIN));
-		echo "年齢を入力：";
-		$b = trim(fgets(STDIN));
-		echo "住所を入力：";
-		$c = trim(fgets(STDIN));
-		$obj = new PersonalData($a, $b, $c);
-		array_push($this->data, $obj);
-	}
-
-	function addBData() {
-		echo "名前を入力：";
-		$a = trim(fgets(STDIN));
-		echo "会社名を入力：";
-		$b = trim(fgets(STDIN));
-		echo "メールアドレスを入力：";
-		$c = trim(fgets(STDIN));
-		echo "電話番号を入力：";
-		$d = trim(fgets(STDIN));
-		$obj = new BusinessData($a, $b, $c, $d);
-		array_push($this->data, $obj);
+	function inputData($type) {
+		switch($type) {
+			case 'f':
+				array_push($this->data, FamilyData::createData());
+				break;
+			case 'p':
+				array_push($this->data, PersonalData::createData());
+				break;
+			case 'b':
+				array_push($this->data, BusinessData::createData());
+				break;
+		}
 	}
 
 	function findIt($fstr) {
 		$flg = false;
 		$result = "";
-		foreach($this->data as $obj) {
+		for($i = 0; $i < count($this->data); $i++) {
+			$obj = $this->data[$i];
 			if (strpos(' ' . $obj->name, $fstr) != null) {
 				$flg = true;
+				echo $i . ":";
 				$obj->writeData();
 			}
 		}
@@ -111,6 +179,22 @@ class MyDataContainer {
 			echo "データが見つかりませんでした。";
 		}
 	}
+
+	function deleteIt($n) {
+		if ($n < 0) { return; }
+		if ($n >= count($this->data)) { return; }
+		array_splice($this->data, $n, 1);
+	}
+
+	function writeList() {
+		for ($i = 0; $i < count($this->data); $i++) {
+			$obj = $this->data[$i];
+			echo $i . " " . $obj->getLabel() . "\n";
+		}
+		echo "\n";
+	}
 }
+
+new MyDataContainer();
 
 ?>
