3f8f58640412380bee750a1a81d191545410c01a
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 22:08:27 +0900

    list 4-31 p.161

--- hello.php
+++ hello.php
@@ -1,7 +1,14 @@
 <?php
 
+function showLines($f) {
+	$lines = @file($f) or exit('ファイルが読み込めませんでした。');
+	for ($i = 0; $i < count($lines); $i++) {
+		echo ($i + 1) . ": " . $lines[$i];
+	}
+}
+
 echo "ファイル名を入力：";
 $a = trim(fgets(STDIN));
-@readfile($a) or exit('※ファイルが読み込めませんでした。');
+showLines($a);
 
 ?>
