51fa0b4c0f4c380579a0c552fcf07d9408dae9dc
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 21:08:59 +0900

    list 4-9 p.136

--- hello.php
+++ hello.php
@@ -1,16 +1,13 @@
 <?php
 
-function change($str, $k) {
-	if ($k == 'u') return strtoupper($str);
-	if ($k == 'l') return strtolower($str);
-	if ($k == 'w') return ucwords($str);
+function changeKana($str, $k) {
+	$str2 = mb_convert_kana($str, $k, 'utf-8');
+	return $str2;
 }
 
 echo "テキストを入力：";
 $a = trim(fgets(STDIN));
-echo "変換キーを入力(u, l, w): ";
-$b = trim(fgets(STDIN));
-echo "[変換済み]\n" . change($a, $b);
+echo "[変換済み]\n" . changeKana($a, 'cCH');
 
 ?>
 
