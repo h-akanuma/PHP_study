679cdc98a89168a0de0fc871057d1b77c11f28f0
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 23:05:56 +0900

    list 4-54 p.187

--- hello.php
+++ hello.php
@@ -1,14 +1,7 @@
 <?php
 
-function countValues($arr) {
-	$arr2 = array_count_values($arr);
-	foreach($arr2 as $key => $value) {
-		echo $key . ": " . $value . "\n";
-	}
-}
-
 echo "項目を入力（カンマで区切って記述）：";
-$a = trim(fgets(STDIN));
-countValues(explode(",", $a));
+$arr = explode(",", trim(fgets(STDIN)));
+echo array_search('PHP', $arr);
 
 ?>
