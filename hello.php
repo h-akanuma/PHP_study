52321795ca85575842ffe718d069a54817102be1
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 22:43:49 +0900

    list 4-43 p.177

--- hello.php
+++ hello.php
@@ -1,7 +1,13 @@
 <?php
 
-echo "フォルダ名を入力：";
+function copyFile($f) {
+	$path = getcwd() . "/backup";
+	if (!file_exists($path)) mkdir ($path);
+	copy(getcwd() . "/" . $f, $path . "/" . $f);
+}
+
+echo "ファイル名を入力：";
 $a = trim(fgets(STDIN));
-mkdir(getcwd() . "/" . $a);
+copyFile($a);
 
 ?>
