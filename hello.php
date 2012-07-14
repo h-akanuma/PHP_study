af51f943065ab42845bb420155a1153b6c8db323
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 23:03:25 +0900

    list 4-53 p.186

--- hello.php
+++ hello.php
@@ -1,14 +1,14 @@
 <?php
 
-function setUnique($arr) {
-	$arr2 = array_unique($arr);
-	foreach($arr2 as $value) {
-		echo $value . " ";
+function countValues($arr) {
+	$arr2 = array_count_values($arr);
+	foreach($arr2 as $key => $value) {
+		echo $key . ": " . $value . "\n";
 	}
 }
 
 echo "項目を入力（カンマで区切って記述）：";
 $a = trim(fgets(STDIN));
-setUnique(explode(",", $a));
+countValues(explode(",", $a));
 
 ?>
