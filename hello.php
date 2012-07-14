a2ca182b0db8f52349c13c6faa15689e0610f5a3
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 23:56:13 +0900

    list 4-64 p.198

--- hello.php
+++ hello.php
@@ -1,8 +1,24 @@
 <?php
 
-$root = simplexml_load_file('data.xml');
-foreach ($root->children() as $data) {
-	echo(mb_convert_encoding($data->name . "," . $data->mail . "," . $data->tel, 'utf-8', 'utf-8') . "\n");
+function addToXMLData($root, $name, $mail, $tel) {
+	$obj = $root->addChild('data');
+	$obj->addChild('name', mb_convert_encoding($name, 'utf-8', 'utf-8'));
+	$obj->addChild('mail', mb_convert_encoding($mail, 'utf-8', 'utf-8'));
+	$obj->addChild('tel', mb_convert_encoding($tel, 'utf-8', 'utf-8'));
 }
+
+function saveXMLData($name, $mail, $tel) {
+	$root = simplexml_load_file('data.xml');
+	addToXMLData($root, $name, $mail, $tel);
+	$root->asXML('data.xml');
+}
+
+echo "名前を入力：";
+$name = trim(fgets(STDIN));
+echo "メールアドレスを入力：";
+$mail = trim(fgets(STDIN));
+echo "電話番号を入力：";
+$tel = trim(fgets(STDIN));
+saveXMLData($name, $mail, $tel);
 
 ?>
