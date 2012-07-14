5ccb57f53fcb8d5a508aea36a40abb5779f24bc5
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 15:04:03 +0900

    list 3-5 p.93

--- hello.php
+++ hello.php
@@ -1,63 +1,76 @@
 <?php
 
-$data = array();
-
-while(true) {
-	echo "検索=1, 入力=2 : ";
-	$a = trim(fgets(STDIN));
-	switch($a) {
-		case 1:
-			echo "検索テキストを入力：";
-			$a = trim(fgets(STDIN));
-			findIt($a);
-			break;
-		case 2:
-			addData();
-			break;
-		default:
-			exit(0);
-	}
-}
+new MyDataContainer();
 
 class MyData {
 	public $name = "";
 	public $mail = "";
 	public $tel = "";
 
+	function __construct($name, $mail, $tel) {
+		$this->name = $name;
+		$this->mail = $mail;
+		$this->tel = $tel;
+	}
+
 	function writeData() {
-		echo $this->name . "\n";
+		echo "[" . $this->name . "]\n";
 		echo "mail: " . $this->mail . "\n";
 		echo "tel:  " . $this->tel . "\n\n";
 	}
 }
 
-function addData() {
-	global $data;
-	echo "名前を入力：";
-	$a = trim(fgets(STDIN));
-	echo "メールアドレスを入力：";
-	$b = trim(fgets(STDIN));
-	echo "電話番号を入力：";
-	$c = trim(fgets(STDIN));
-	$obj = new MyData();
-	$obj->name = $a;
-	$obj->mail = $b;
-	$obj->tel = $c;
-	array_push($data, $obj);
-}
+class MyDataContainer {
+	public $data = null;
 
-function findIt($fstr) {
-	global $data;
-	$flg = false;
-	$result = "";
-	foreach($data as $obj) {
-		if (strpos(' ' . $obj->name, $fstr) != null) {
-			$flg = true;
-			$obj->writeData();
+	function __construct() {
+		$this->data = array();
+		$this->start();
+	}
+
+	function start() {
+		while(true) {
+			echo "検索=1, 入力=2 : ";
+			$a = trim(fgets(STDIN));
+			switch($a) {
+				case 1:
+					echo "検索テキストを入力：";
+					$a = trim(fgets(STDIN));
+					$this->findIt($a);
+					break;
+				case 2:
+					$this->addData();
+					break;
+				default:
+					exit(0);
+			}
+			echo "\n";
 		}
 	}
-	if ($flg == false) {
-		echo "データが見つかりませんでした。";
+
+	function addData() {
+		echo "名前を入力：";
+		$a = trim(fgets(STDIN));
+		echo "メールアドレスを入力：";
+		$b = trim(fgets(STDIN));
+		echo "電話番号を入力：";
+		$c = trim(fgets(STDIN));
+		$obj = new MyData($a, $b, $c);
+		array_push($this->data, $obj);
+	}
+
+	function findIt($fstr) {
+		$flg = false;
+		$result = "";
+		foreach($this->data as $obj) {
+			if (strpos(' ' . $obj->name, $fstr) != null) {
+				$flg = true;
+				$obj->writeData();
+			}
+		}
+		if ($flg == false) {
+			echo "データが見つかりませんでした。";
+		}
 	}
 }
 
