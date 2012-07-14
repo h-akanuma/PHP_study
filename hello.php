e6d7e2ee356b8320778f33eea5b02faf8ebd792b
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 13:56:31 +0900

    list 2-11 p.70

--- hello.php
+++ hello.php
@@ -6,19 +6,38 @@ $data = array(
 		'hanako' => 'hanako@flower.jp'
 );
 
-$flg = false;
-
-echo "名前を入力：";
-$a = trim(fgets(STDIN));
-foreach ($data as $key => $val) {
-	if (strpos(' ' . $key, $a) != null) {
-		$flg = true;
-		echo $key . ": " . $val;
+while(true) {
+	echo "検索=1, 入力=2 :";
+	$a = trim(fgets(STDIN));
+	switch($a) {
+		case 1:
+			$flg = false;
+			echo "名前を入力：";
+			$a = trim(fgets(STDIN));
+			foreach($data as $key => $val) {
+				if (strpos(' ' . $key , $a) != null) {
+					$flg = true;
+					echo $key . ": " . $val . "\n";
+				}
+			}
+			if ($flg == false) {
+				echo "データが見つかりませんでした。";
+			}
+			break;
+		case 2:
+			echo "検索文字を入力：";
+			$a = trim(fgets(STDIN));
+			echo "メールアドレスを入力：";
+			$b = trim(fgets(STDIN));
+			$data[$a] = $b;
+			break;
+		default:
+			exit(0);
 	}
+	echo "\n";
 }
 
-if($flg == null) {
-	echo "データが見つかりませんでした。";
-}
+?>
+
 
 ?>
