d73b2856941a658177111d75f8e8451a754e452e
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 21:29:21 +0900

    list 4-17 p.144

--- hello.php
+++ hello.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "日付を入力(yyyy,mm,dd): ";
-$a = explode(',', trim(fgets(STDIN)));
-echo "タイムスタンプ：" . mktime(0, 0, 0, $a[1], $a[2], $a[0]);
+echo "日付を入力(yyyy-mm-dd): ";
+$a = trim(fgets(STDIN));
+echo "タイムスタンプ：" . strtotime($a);
 
 ?>
