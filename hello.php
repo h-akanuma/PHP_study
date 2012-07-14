54e4f8b9cd6da25cdb86beb84550c4462afd9c76
h-akanuma <hiroaki.akanuma@gmail.com>
2012-07-14 23:37:36 +0900

    list4-60 p.193

--- hello.php
+++ hello.php
@@ -1,17 +1,13 @@
 <?php
 
-function addToCsv($name, $mail, $tel) {
-	$f = @fopen('data.csv', 'ab') or exit('ファイルが開けません。');
-	fputcsv($f, array($name, $mail, $tel));
+function findCsv($str) {
+	$f = @fopen('data.csv', 'rb') or exit('ファイルが開けません。');
+	while($data = fgetcsv($f, 1024)) if ($data[0] == $str) print_r ($data);
 	fclose($f);
 }
 
-echo "名前を入力:";
+echo "検索テキストを入力：";
 $a = trim(fgets(STDIN));
-echo "メールアドレスを入力：";
-$b = trim(fgets(STDIN));
-echo "電話番号を入力：";
-$c = trim(fgets(STDIN));
-addToCsv($a, $b, $c);
+findCsv($a);
 
 ?>
